0cf270284f9875872fb74189e5830575c18aa097
spifory <luvuyosilubane50@gmail.com>
2023-08-15 22:12:11 +0200

    feat(bot): add preview command and modal

---
+++ bot/src/plugins/__main__.py
@@ -0,0 +1,39 @@
+"""The main file for the bot's commands."""
+
+from logging import getLogger
+
+from crescent import Context, Plugin, command
+from hikari import RESTBot, TextInputStyle
+
+plugin = Plugin[RESTBot, None]()
+LOG = getLogger(__name__)
+
+TAGS_PREVIEW = """
+<meta name="title" content="name of the site" />
+<meta name="description" content="the site!" />
+"""
+
+
+@plugin.include
+@command(description="Preview what your Meta/OpenGraph tags will look like on Discord.")
+async def preview(ctx: Context) -> None:
+    modal = plugin.app.rest.build_modal_action_row()
+
+    modal.add_text_input(
+        f"preview_modal_content:{ctx.user.id}", # custom_id, positional for some reason
+        "Tags", # label
+        style=TextInputStyle.PARAGRAPH,
+        placeholder=TAGS_PREVIEW,
+    )
+
+    return await ctx.interaction.create_modal_response(
+        title="Tags Preview", custom_id=f"preview_modal:{ctx.user.id}", component=modal
+    )
+
+@plugin.load_hook
+def load() -> None:
+    LOG.info("Loaded %s", __name__)
+
+@plugin.unload_hook
+def unload() -> None:
+    LOG.warning("Unloaded %s", __name__)
