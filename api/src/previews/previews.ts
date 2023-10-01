e6b94f9b108a32af274b76f9c99a5c90f0b42112
spifory <luvuyosilubane50@gmail.com>
2023-10-01 12:40:59 +0200

    docs(previews): document the `Preview` model

--- api/src/previews/previews.ts
+++ api/src/previews/previews.ts
@@ -1,5 +1,13 @@
+/**
+ * The preview model/object containing all the data in a preview.
+ */
 export interface Preview {
+    /** The ID of the preview. This cannot be modified/added by the user. */
     id: string;
+    /** The author of this preview. */
     authorID: string;
+    /**
+     * The preview's content. This contains the Meta/OG tags.
+     */
     content: string;
 }
